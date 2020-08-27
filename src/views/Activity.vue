43cb3c4e2107b184982457297d60e83833d07a57
Bestian Tang <bestian@gmail.com>
2020-08-27 16:46:36 +0800

    Update Activity.vue

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -5,12 +5,13 @@
             我愛玩學校活動
         </a>
     </h1>
-    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F105527050952705%2Fvideos%2F638153220155025%2F&show_text=false&width=400&appId=485195848253155&height=600" width="400" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+    <iframe class ="load" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F105527050952705%2Fvideos%2F638153220155025%2F&show_text=false&width=400&appId=485195848253155&height=600" width="400" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F%25E6%2588%2591-%25E6%2584%259B-%25E7%258E%25A9-%25E5%25AD%25B8-%25E6%25A0%25A1-105527050952705&tabs=events&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=485195848253155" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
   </div>
 </template>
 
 <style type="text/css" scoped="">
-iframe {
-  background: url('../assets/loading.gif') no-repeat center;}
+iframe.load {
+  background: url('../assets/loading.gif') no-repeat center;
+}
 </style>
