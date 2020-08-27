80816687b9304fb8b57a2517ac23a3eeab74a8a2
Bestian Tang <bestian@gmail.com>
2020-08-27 08:33:21 +0800

    Update Activity.vue

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -5,5 +5,6 @@
             我愛玩學校活動
         </a>
     </h1>
+    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F105527050952705%2Fvideos%2F638153220155025%2F&show_text=false&width=400&appId=485195848253155&height=600" width="400" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
   </div>
 </template>
