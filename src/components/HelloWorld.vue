b645149e7f98f8ed3121c237fe613f34924fe2f7
Bestian Tang <bestian@gmail.com>
2020-08-27 08:50:34 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2F3dw.github.io%2Filove%2F%23%2F&layout=button_count&size=large&appId=485195848253155&width=80&height=28" width="80" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <br/>
     <router-link to="/people">
       <img src="../assets/main2.jpg">
     </router-link>
