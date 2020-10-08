bc4ab2f430133d75a10ce86ec506b3c2793c4084
Bestian Tang 小巴 <bestian@gmail.com>
2020-10-09 07:56:09 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import Vue2TouchEvents from 'vue2-touch-events'
- 
+import './registerServiceWorker'
+
 Vue.use(Vue2TouchEvents)
 
 Vue.config.productionTip = false
