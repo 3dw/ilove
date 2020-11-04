89dc7c5002736234bca233283d2f1e24fcb25e90
Bestian Tang 小巴 <bestian@gmail.com>
2020-11-04 08:10:22 +0800

    test vue-gtag

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import './registerServiceWorker'
 import router from './router'
 import Vue2TouchEvents from 'vue2-touch-events'
 import './registerServiceWorker'
+import VueGtag from 'vue-gtag'
 
 Vue.use(Vue2TouchEvents)
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "G-FWC2WSJYJ9" }
+}, router)
 
 new Vue({
   router,
